45a590dff8933ff8abc318e878b770bdd5816ede
kj-dev <agassi77.dev@gmail.com>
2022-11-10 18:04:41 +0900

    useQuery

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,30 +1,47 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  query MoviesQuery {
+    allMovies {
+      id
+      title
+    }
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
+  }
+`;
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              id
-              title
-            }
-          }
-        `,
-      })
-      .then((results) => setMovies(results.data.allMovies));
-  }, [client]);
-  console.log(movies);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
   return (
-    <ul>
-      {movies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-    </ul>
+    <div>
+      <h1>Movies</h1>
+      <ul>
+        {data.allMovies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+
+      <h1>Tweets</h1>
+      <ul>
+        {data.allTweets.map((tweet) => (
+          <li key={tweet.id}>
+            {tweet.text} by {tweet.author.fullName}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
